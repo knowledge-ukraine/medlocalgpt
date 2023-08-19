1e8f5422834869f7b9611e2aa40487de83494f4a
Kyrylo Malakhov <malakhovks@nas.gov.ua>
2023-08-19 15:21:32 +0300

    fix imports
    fix imports

--- ingest.py
+++ ingest.py
@@ -5,7 +5,7 @@ from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_compl
 import click
 import torch
 from langchain.docstore.document import Document
-from langchain.embeddings import HuggingFaceInstructEmbeddings
+# from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.text_splitter import Language, RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 
