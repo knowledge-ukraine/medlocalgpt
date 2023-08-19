3353ec788511b5d8d48f652a8d996b266d10c91e
Kyrylo Malakhov <malakhovks@nas.gov.ua>
2023-08-19 17:54:31 +0300

    fix bash start
    fix bash start

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@
 export TRANSFORMERS_CACHE=/var/tmp/hf/models
 export HF_HOME=/var/tmp/hf/misc
 export HF_DATASETS_CACHE=/var/tmp/hf/datasets
-export SENTENCE_TRANSFORMERS_HOME=/var/tmp/hf/sentence'
+export SENTENCE_TRANSFORMERS_HOME=/var/tmp/hf/sentence
 python ingest.py --device_type cpu && uwsgi --ini ./deploy/uwsgi.ini
