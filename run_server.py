02a000a7fb28a14baac5a5dfe4b6abe97232f0c2
Kyrylo Malakhov <malakhovks@nas.gov.ua>
2023-08-21 10:18:52 +0300

    add lang properties
    add lang properties

--- run_server.py
+++ run_server.py
@@ -3,7 +3,7 @@ import os, json
 import shutil
 import subprocess
 
-import torch
+# import torch
 from auto_gptq import AutoGPTQForCausalLM
 from huggingface_hub import hf_hub_download
 from flask import Flask, jsonify, request
@@ -42,94 +42,6 @@ SHOW_SOURCES = True
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 logging.info(f"Running on: {DEVICE_TYPE}")
 logging.info(f"Display Source Documents set to: {SHOW_SOURCES}")
-
-
-def load_model(device_type, model_id, model_basename=None):
-    logging.info(f"Loading Model: {model_id}, on: {device_type}")
-    logging.info("This action can take a few minutes!")
-
-    if model_basename is not None:
-        if ".ggml" in model_basename:
-            logging.info("Using Llamacpp for GGML quantized models")
-            model_path = hf_hub_download(repo_id=model_id, filename=model_basename)
-            max_ctx_size = 2048
-            kwargs = {
-                "model_path": model_path,
-                "n_ctx": max_ctx_size,
-                "max_tokens": max_ctx_size,
-            }
-            if device_type.lower() == "mps":
-                kwargs["n_gpu_layers"] = 1000
-            if device_type.lower() == "cuda":
-                kwargs["n_gpu_layers"] = 1000
-                kwargs["n_batch"] = max_ctx_size
-            return LlamaCpp(**kwargs)
-
-        else:
-            # The code supports all huggingface models that ends with GPTQ and have some variation
-            # of .no-act.order or .safetensors in their HF repo.
-            logging.info("Using AutoGPTQForCausalLM for quantized models")
-
-            if ".safetensors" in model_basename:
-                # Remove the ".safetensors" ending if present
-                model_basename = model_basename.replace(".safetensors", "")
-
-            tokenizer = AutoTokenizer.from_pretrained(model_id, use_fast=True)
-            logging.info("Tokenizer loaded")
-
-            model = AutoGPTQForCausalLM.from_quantized(
-                model_id,
-                model_basename=model_basename,
-                use_safetensors=True,
-                trust_remote_code=True,
-                device="cuda:0",
-                use_triton=False,
-                quantize_config=None,
-            )
-    elif (
-        device_type.lower() == "cuda"
-    ):  # The code supports all huggingface models that ends with -HF or which have a .bin
-        # file in their HF repo.
-        logging.info("Using AutoModelForCausalLM for full models")
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-        logging.info("Tokenizer loaded")
-
-        model = AutoModelForCausalLM.from_pretrained(
-            model_id,
-            device_map="auto",
-            torch_dtype=torch.float16,
-            low_cpu_mem_usage=True,
-            trust_remote_code=True,
-            # max_memory={0: "15GB"} # Uncomment this line with you encounter CUDA out of memory errors
-        )
-        model.tie_weights()
-    else:
-        logging.info("Using LlamaTokenizer")
-        tokenizer = LlamaTokenizer.from_pretrained(model_id)
-        model = LlamaForCausalLM.from_pretrained(model_id)
-
-    # Load configuration from the model to avoid warnings
-    generation_config = GenerationConfig.from_pretrained(model_id)
-    # see here for details:
-    # https://huggingface.co/docs/transformers/
-    # main_classes/text_generation#transformers.GenerationConfig.from_pretrained.returns
-
-    # Create a pipeline for text generation
-    pipe = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        max_length=2048,
-        temperature=0,
-        top_p=0.95,
-        repetition_penalty=1.15,
-        generation_config=generation_config,
-    )
-
-    local_llm = HuggingFacePipeline(pipeline=pipe)
-    logging.info("Local LLM Loaded")
-
-    return local_llm
 
 DB = Chroma(
     persist_directory=PERSIST_DIRECTORY,
@@ -260,14 +172,16 @@ def prompt_route():
     else:
         return "No user prompt received", 400
 
-@app.route("/medlocalgpt/api/v1/gt/uk-en/ask", methods=["GET", "POST"])
-def prompt_uk_en():
+@app.route("/medlocalgpt/api/v1/gt/ask", methods=["GET", "POST"])
+def prompt_gt():
     global QA
     translator = Translator()
 
     #  template = """You are an AI assistant for answering questions about {subject}. Provide a very detailed comprehensive academic answer. If you don't know the answer, just say "I'm not sure." Don't try to make up an answer. If the question is not about {subject} and not directly in the given context, politely inform them that you are tuned to only answer questions about {subject}. Question: {question} ========= {context} ========= Answer:"""
     
     use_model = request.args.get('model', default = 'local', type = str)
+    lang_src = request.args.get('lang_src', default = 'uk', type = str)
+    lang_dest = request.args.get('lang_dest', default = 'en', type = str)
 
     if use_model == 'openai':
         QA = RetrievalQA.from_chain_type(llm=LLM_OPENAI, chain_type="stuff", retriever=RETRIEVER, return_source_documents=SHOW_SOURCES)
@@ -278,7 +192,8 @@ def prompt_uk_en():
     if user_prompt:
         #Translation uk to en
         # logging.info(translator.translate(user_prompt, src='uk', dest='en'))
-        tr_prompt = translator.translate(user_prompt, src='uk', dest='en')
+        # tr_prompt = translator.translate(user_prompt, src='uk', dest='en')
+        tr_prompt = translator.translate(user_prompt, src=lang_src, dest=lang_dest)
         # Get the answer from the chain
         res = QA(tr_prompt.text)
         answer, docs = res["result"], res["source_documents"]
