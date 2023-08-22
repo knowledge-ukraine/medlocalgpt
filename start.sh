bb5284e9e8d278c0585f313d0883b1673628572a
Kyrylo Malakhov <malakhovks@nas.gov.ua>
2023-08-22 12:58:43 +0300

    Check if Chroma index exists with Bash
    Check if Chroma index exists with Bash

--- start.sh
+++ start.sh
@@ -4,4 +4,13 @@ export TRANSFORMERS_CACHE=/var/tmp/hf/models
 export HF_HOME=/var/tmp/hf/misc
 export HF_DATASETS_CACHE=/var/tmp/hf/datasets
 export SENTENCE_TRANSFORMERS_HOME=/var/tmp/hf/sentence
-python ingest.py --device_type cpu && uwsgi --ini ./deploy/uwsgi.ini
+
+if [[ -d ./DB && -n "$(ls -A ./DB)" ]]; then 
+  echo "Chroma index exists."
+  uwsgi --ini ./deploy/uwsgi.ini
+else
+  echo "Chroma index does not exist. Let's create it!"
+  python ingest.py --device_type cpu && uwsgi --ini ./deploy/uwsgi.ini
+fi
+
+# python ingest.py --device_type cpu && uwsgi --ini ./deploy/uwsgi.ini
