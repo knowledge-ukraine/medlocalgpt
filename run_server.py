807c204156d0ab062bd9f6685e8bd69a19a7350f
Kyrylo Malakhov <malakhovks@nas.gov.ua>
2023-08-24 19:30:46 +0300

    fix prompt
    fix prompt

--- run_server.py
+++ run_server.py
@@ -146,7 +146,6 @@ template = """The subject areas of your responses should be: {subject}. The doma
 # prompt = PromptTemplate.from_template(template)
 prompt = PromptTemplate(input_variables=["history", "context", "question", "subject"], template=template)
 subject = "medicine, physical rehabilitation medicine, telerehabilitation, cardiovascular system, arterial oscillography, health informatics, digital health, computer sciences, transdisciplinary research"
-prompt.partial(subject=subject)
 
 memory = ConversationBufferMemory(input_key="question", memory_key="history", return_messages=True)
 
@@ -168,7 +167,7 @@ if OPENAI_API_KEY and OPENAI_ORGANIZATION is not None:
     LLM_OPENAI = ChatOpenAI(model='gpt-3.5-turbo-16k', max_tokens=2024, openai_api_key=OPENAI_API_KEY, openai_organization=OPENAI_ORGANIZATION)
     QA_OPENAI = RetrievalQA.from_chain_type(
         llm=LLM_OPENAI, chain_type="stuff", retriever=RETRIEVER, return_source_documents=SHOW_SOURCES,
-        chain_type_kwargs={"prompt": prompt, "memory": memory}
+        chain_type_kwargs={"prompt": prompt.partial(subject=subject), "memory": memory}
     )
 
 app = Flask(__name__)
