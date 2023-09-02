83dfb2a11045126471e5c4c837984d7f7bf90c83
Kyrylo Malakhov <malakhovks@nas.gov.ua>
2023-09-02 19:29:41 +0300

    fix index
    fix index

--- run_server.py
+++ run_server.py
@@ -475,7 +475,7 @@ def process_en_dataset_local_query_v1():
 
 @app.route('/')
 def index():
-    return 'index'
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
