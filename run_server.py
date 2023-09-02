3bb9b8f3c5547c7b3f482cc66a345eda565bf525
Kyrylo Malakhov <malakhovks@nas.gov.ua>
2023-09-02 12:13:36 +0300

    clean up code
    clean up code

--- run_server.py
+++ run_server.py
@@ -181,73 +181,73 @@ b'_5#y2L"F4Q8z\n\xec]/'
 """
 app.secret_key = os.urandom(42)
 
-@app.route("/medlocalgpt/api/v1/delete_source", methods=["GET"])
-def delete_source_route():
-    folder_name = "SOURCE_DOCUMENTS"
+# @app.route("/medlocalgpt/api/v1/delete_source", methods=["GET"])
+# def delete_source_route():
+#     folder_name = "SOURCE_DOCUMENTS"
 
-    if os.path.exists(folder_name):
-        shutil.rmtree(folder_name)
+#     if os.path.exists(folder_name):
+#         shutil.rmtree(folder_name)
 
-    os.makedirs(folder_name)
+#     os.makedirs(folder_name)
 
-    return jsonify({"message": f"Folder '{folder_name}' successfully deleted and recreated."})
+#     return jsonify({"message": f"Folder '{folder_name}' successfully deleted and recreated."})
 
 # add route to delete PERSIST_DIRECTORY
 
-@app.route("/medlocalgpt/api/v1/admin/save_document", methods=["GET", "POST"])
-def save_document_route():
-    if "document" not in request.files:
-        return "No document part", 400
-    file = request.files["document"]
-    if file.filename == "":
-        return "No selected file", 400
-    if file:
-        filename = secure_filename(file.filename)
-        folder_path = "SOURCE_DOCUMENTS"
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-        file_path = os.path.join(folder_path, filename)
-        file.save(file_path)
-        return "File saved successfully", 200
+# @app.route("/medlocalgpt/api/v1/admin/save_document", methods=["GET", "POST"])
+# def save_document_route():
+#     if "document" not in request.files:
+#         return "No document part", 400
+#     file = request.files["document"]
+#     if file.filename == "":
+#         return "No selected file", 400
+#     if file:
+#         filename = secure_filename(file.filename)
+#         folder_path = "SOURCE_DOCUMENTS"
+#         if not os.path.exists(folder_path):
+#             os.makedirs(folder_path)
+#         file_path = os.path.join(folder_path, filename)
+#         file.save(file_path)
+#         return "File saved successfully", 200
 
 
-@app.route("/medlocalgpt/api/v1/admin/ingest", methods=["GET"])
-def run_ingest_route():
-    global DB
-    global RETRIEVER
-    global QA
+# @app.route("/medlocalgpt/api/v1/admin/ingest", methods=["GET"])
+# def run_ingest_route():
+#     global DB
+#     global RETRIEVER
+#     global QA
 
-    try:
-        if os.path.exists(PERSIST_DIRECTORY):
-            try:
-                shutil.rmtree(PERSIST_DIRECTORY)
-            except OSError as e:
-                logging.error(f"Error: {e.filename} - {e.strerror}.")
-        else:
-            logging.info(PERSIST_DIRECTORY + " directory does not exist")
+#     try:
+#         if os.path.exists(PERSIST_DIRECTORY):
+#             try:
+#                 shutil.rmtree(PERSIST_DIRECTORY)
+#             except OSError as e:
+#                 logging.error(f"Error: {e.filename} - {e.strerror}.")
+#         else:
+#             logging.info(PERSIST_DIRECTORY + " directory does not exist")
 
-        run_langest_commands = ["python", "ingest.py"]
-        if DEVICE_TYPE == "cpu":
-            run_langest_commands.append("--device_type")
-            run_langest_commands.append(DEVICE_TYPE)
+#         run_langest_commands = ["python", "ingest.py"]
+#         if DEVICE_TYPE == "cpu":
+#             run_langest_commands.append("--device_type")
+#             run_langest_commands.append(DEVICE_TYPE)
             
-        result = subprocess.run(run_langest_commands, capture_output=True)
-        if result.returncode != 0:
-            return "Script execution failed: {}".format(result.stderr.decode("utf-8")), 500
-        # load the vectorstore
-        DB = Chroma(
-            persist_directory=PERSIST_DIRECTORY,
-            embedding_function=EMBEDDINGS,
-            client_settings=CHROMA_SETTINGS,
-        )
-        RETRIEVER = DB.as_retriever(5)
+#         result = subprocess.run(run_langest_commands, capture_output=True)
+#         if result.returncode != 0:
+#             return "Script execution failed: {}".format(result.stderr.decode("utf-8")), 500
+#         # load the vectorstore
+#         DB = Chroma(
+#             persist_directory=PERSIST_DIRECTORY,
+#             embedding_function=EMBEDDINGS,
+#             client_settings=CHROMA_SETTINGS,
+#         )
+#         RETRIEVER = DB.as_retriever(5)
 
-        QA = RetrievalQA.from_chain_type(
-            llm=LLM_LOCAL, chain_type="stuff", retriever=RETRIEVER, return_source_documents=SHOW_SOURCES
-        )
-        return "Script executed successfully: {}".format(result.stdout.decode("utf-8")), 200
-    except Exception as e:
-        return f"Error occurred: {str(e)}", 500
+#         QA = RetrievalQA.from_chain_type(
+#             llm=LLM_LOCAL, chain_type="stuff", retriever=RETRIEVER, return_source_documents=SHOW_SOURCES
+#         )
+#         return "Script executed successfully: {}".format(result.stdout.decode("utf-8")), 200
+#     except Exception as e:
+#         return f"Error occurred: {str(e)}", 500
 
 # Tuning prompt (with selected domain knowledge) for query to OpenAI model in English
 @app.route("/medlocalgpt/api/v1/en/advanced/openai/ask", methods=["GET", "POST"])
@@ -472,127 +472,6 @@ def process_en_dataset_local_query_v1():
         return jsonify(prompt_response_dict), 200
     else:
         return "No user prompt received", 400
-
-# @app.route("/medlocalgpt/api/v1/ask", methods=["GET", "POST"])
-# def prompt_route():
-#     # global QA
-
-#     use_model = request.args.get('model', default = 'local', type = str)
-#     user_prompt = request.form.get("prompt")
-#     lang_src = request.args.get('lang_src', default = 'Ukrainian', type = str)
-#     lang_dest = request.args.get('lang_dest', default = 'English', type = str)
-
-#     if use_model == 'openai':
-#         if OPENAI_API_KEY and OPENAI_ORGANIZATION is not None:
-#             system_message_prompt_template = SystemMessagePromptTemplate.from_template(
-#                     SYSTEM_TEMPLATE_FOR_TRANSLATION
-#                 )
-#             human_template = "{sample_text}"
-#             human_message_prompt_template = HumanMessagePromptTemplate.from_template(human_template)
-#             chat_prompt_template = ChatPromptTemplate.from_messages(
-#                     [system_message_prompt_template, human_message_prompt_template]
-#                 )
-#             # initialize LLMChain by passing LLM and prompt template
-#             llm_chain_1 = LLMChain(llm=LLM_OPENAI, prompt=chat_prompt_template)
-
-#             qa_openai = RetrievalQA.from_chain_type(
-#                     llm=LLM_OPENAI, chain_type="stuff", retriever=RETRIEVER, return_source_documents=SHOW_SOURCES,
-#                     chain_type_kwargs={"prompt": prompt.partial(subject=SUBJECT), "memory": memory}
-#                 )
-#             qa = qa_openai
-
-#             logging.debug('Use QA_OPENAI')
-#         else:
-#             return "No OPENAI cridentials received", 400
-#     if use_model == 'local':
-#         qa = QA_LOCAL
-#         logging.debug('Use QA_LOCAL')
-
-#     if user_prompt:
-#         logging.debug('Get the answer from the chain')
-
-#         tr = llm_chain_1.run(input_lang=lang_src, output_lang=lang_dest, subject=SUBJECT,sample_text=user_prompt)
-#         logging.debug(f"Translation Uk-En: {tr}")
-
-#         res = qa(tr)
-
-#         answer, docs = res["result"], res["source_documents"]
-
-#         tr = llm_chain_1.run(input_lang=lang_dest, output_lang=lang_src, subject=SUBJECT,sample_text=answer)
-
-#         logging.debug(f"Translation En-Uks: {tr}")
-
-#         prompt_response_dict = {
-#             "Prompt": user_prompt,
-#             "Answer": tr,
-#         }
-
-#         prompt_response_dict["Sources"] = []
-#         for document in docs:
-#             prompt_response_dict["Sources"].append(
-#                 (os.path.basename(str(document.metadata["source"])), 'https://cdn.e-rehab.pp.ua/u/' + re.sub(r"\s+", '%20', os.path.basename(str(document.metadata["source"]))), str(document.page_content))
-#             )
-
-#         logging.debug('RESULTS:' + json.dumps(prompt_response_dict, indent=4))
-
-#         return jsonify(prompt_response_dict), 200
-#     else:
-#         return "No user prompt received", 400
-
-# @app.route("/medlocalgpt/api/v1/gt/dataset/ask", methods=["GET", "POST"])
-# def process_gt_dataset_query_v1():
-#     # global QA
-#     translator = Translator()
-
-#     use_model = request.args.get('model', default = 'local', type = str)
-#     lang_src = request.args.get('lang_src', default = 'uk', type = str)
-#     lang_dest = request.args.get('lang_dest', default = 'en', type = str)
-
-#     if use_model == 'openai':
-#         if OPENAI_API_KEY and OPENAI_ORGANIZATION is not None:
-#             logging.debug('Use QA_OPENAI')
-#             qa_openai = RetrievalQA.from_chain_type(
-#                     llm=LLM_OPENAI, chain_type="stuff", retriever=RETRIEVER, return_source_documents=SHOW_SOURCES,
-#                     chain_type_kwargs={"prompt": prompt.partial(subject=SUBJECT), "memory": memory}
-#                 )
-#             qa = qa_openai
-#         else:
-#             return "No OPENAI cridentials received", 400
-#     if use_model == 'local':
-#         logging.debug('Use QA_LOCAL')
-#         qa = QA_LOCAL
-
-#     user_prompt = request.form.get("prompt")
-#     if user_prompt:
-#         #Translation uk to en
-#         # logging.info(translator.translate(user_prompt, src='uk', dest='en'))
-#         # tr_prompt = translator.translate(user_prompt, src='uk', dest='en')
-#         logging.debug('Translation from ' + lang_src + ' to ' + lang_dest)
-#         tr_prompt = translator.translate(user_prompt, src=lang_src, dest=lang_dest)
-#         logging.debug('Get the answer from the chain')
-#         res = qa(tr_prompt.text)
-#         answer, docs = res["result"], res["source_documents"]
-#         #Translation en to uk
-#         logging.debug('Translation from en to uk')
-#         tr_response = translator.translate(answer, src='en', dest='uk')
-
-#         prompt_response_dict = {
-#             "Prompt": user_prompt,
-#             "Answer": tr_response.text,
-#         }
-
-#         prompt_response_dict["Sources"] = []
-#         for document in docs:
-#             prompt_response_dict["Sources"].append(
-#                 (os.path.basename(str(document.metadata["source"])), 'https://cdn.e-rehab.pp.ua/u/' + re.sub(r"\s+", '%20', os.path.basename(str(document.metadata["source"]))), str(document.page_content))
-#             )
-
-#         logging.debug('RESULTS:' + json.dumps(prompt_response_dict, indent=4))
-
-#         return jsonify(prompt_response_dict), 200
-#     else:
-#         return "No user prompt received", 400
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
