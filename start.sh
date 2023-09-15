0058668ef51a678523affc25d972b8a505e76822
Kyrylo Malakhov <malakhovks@nas.gov.ua>
2023-09-15 09:53:32 +0300

    fix deploy err
    fix deploy err

--- start.sh
+++ start.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env bash
 
-export TRANSFORMERS_CACHE=/var/tmp/hf/models
-export HF_HOME=/var/tmp/hf/misc
-export HF_DATASETS_CACHE=/var/tmp/hf/datasets
-export SENTENCE_TRANSFORMERS_HOME=/var/tmp/hf/sentence
-
-if [[ -d ./DB && -n "$(ls -A ./DB)" ]]; then 
-  echo "start.sh: Chroma index exists. If you want to build a new index plese remove DB directory."
-  uwsgi --ini ./deploy/uwsgi.ini
-else
-  echo "start.sh: Chroma index does not exist. Let's create it!"
-  python ingest.py --device_type cpu && uwsgi --ini ./deploy/uwsgi.ini
-fi
-
-# python ingest.py --device_type cpu && uwsgi --ini ./deploy/uwsgi.ini
+uwsgi --ini ./deploy/uwsgi.ini
